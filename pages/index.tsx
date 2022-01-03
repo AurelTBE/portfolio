f23bedeb208ff820447e598c1c0c2c3ea030ae7c
aureltbe <aureltrouble@gmail.com>
2022-01-03 15:30:45 +0100

    Added dependencies and tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,9 @@ const Home: NextPage = () => {
           Get started by editing{' '}
           <code className={styles.code}>pages/index.tsx</code>
         </p>
-
+        <div className="flex m-20">
+          Coucou
+        </div>
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>

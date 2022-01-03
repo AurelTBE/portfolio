316d2915fb7310cd496abe9ddee830fa61d7210b
aureltbe <aureltrouble@gmail.com>
2022-01-03 16:46:45 +0100

    Start 0.13

---
+++ components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from 'next/link'
+import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai'
+import { DiCssdeck } from 'react-icons/di'
+
+interface Props {
+    
+}
+
+export const Header = () => {
+    return (
+        <div>
+            Header
+        </div>
+    )
+}

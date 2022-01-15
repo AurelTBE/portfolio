fe399ed704778465e8933db4644b0116a6da2fdf
AurelTBE <aureltrouble@gmail.com>
2022-01-15 18:22:32 +0100

    ProjectCard 0.1

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -12,20 +12,21 @@ export interface IProject {
 
 
 const ProjectCard = ({project}: {project:IProject}) => {
+    const {title, image, description, tags, source, visit, id} = project
     return (
         <div className="">
             <div className='relative mb-6 overflow-hidden shadow-md pb-80'>
                 <img
-                    alt={project.title}
-                    src={project.image}
+                    alt={title}
+                    src={image}
                     className='absolute object-cover object-top w-full rounded-t-lg shadow-lg h-80 lg:rounded-lg'
                 />
             </div>
             <h3 className='mb-2 font-semibold text-xl'>
-                {project.title}
+                {title}
             </h3>
             <div>
-                {project.description}
+                {description}
             </div>
         </div>
     )

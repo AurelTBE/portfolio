5bdd3760c73a23a46faa8b369853c56139a64779
AurelTBE <aureltrouble@gmail.com>
2022-01-15 12:52:24 +0100

    Projects WIP0.2

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -7,17 +7,16 @@ interface Props {
 
 const Projects = (props: Props) => {
     return (
-        <section id="projets">
-            <div className="text-white grid grid-cols-3 gap-4">
+        <section id="projets" className="mx-10 lg:mx-20">
+            <div className="text-white grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
                 {projects.map(project => (
-                    <div className="grid items-center justify-center">
-                        <h2>
+                    <div className="">
+                        <h2 className='mb-2 font-semibold text-xl'>
                             {project.title}
                         </h2>
                         <div>
                             {project.description}
                         </div>
-                        Coucou
                     </div>
                 ))}
             </div>

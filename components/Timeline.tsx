2fda9fdc90ad9f2c846bb1e8c8c402a1acd6815f
aureltbe <aureltrouble@gmail.com>
2022-01-18 17:45:30 +0100

    Timeline design wip 0.3

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -24,20 +24,21 @@ const Timeline = (props: Props) => {
                 <h3 className="text-3xl text-white">
                     A propos de moi
                 </h3>
-                <div className='relative container mx-auto px-6 my-6'>
-                    <div className="absolute z-0 w-2 h-full bg-white shadow-md left-17 inset-0">
-                    </div>
+                <div className='relative container mx-auto px-6 my-6 flex flex-col space-y-8'>
+                    <div 
+                        className="absolute z-0 w-2 h-full bg-white shadow-md left-17 inset-0 md:mx-auto md:left-0 md:right-0"
+                    ></div>
                     {TimeLineData.map(element => (
-                        <div className='relative z-10 py-5'>
-                            <div className='relative pt-12'>
+                        <div className='relative z-10'>
+                            <div className='relative pt-12 xs:absolute'>
                                 <div className='absolute flex left-6 inset-0 items-center justify-center text-3xl text-white bg-pink-800 w-12 h-12 rounded-full border-2 border-white shadow-xl'>
                                     {iconSelect(element.icon)}
                                 </div>
                             </div>
-                            <div className='relative pt-2'>
+                            <div className='relative pt-2 xs:pl-20 xs:pt-0'>
                                 <div 
                                     aria-hidden="true" 
-                                    className='absolute inset-0 left-10 h-4 w-4 transform rotate-45 bg-white'
+                                    className='absolute inset-0 left-10 h-4 w-4 transform rotate-45 bg-white xs:top-4 xs:left-19'
                                 ></div>
                                 <div className='bg-white p-6 rounded-md shadow-md'>
                                     <h4 className='text-lg font-semibold'>{element.title}</h4>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 module.exports = {
   content: ['./components/**/*.tsx', './pages/**/*.tsx','./public/**/*.html'],
   theme: {
+    screens: {
+      'xs': '480px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       inset: {
-        "17": "68px"
+        "17": "68px",
+        "18": "72px",
+        "19": "76px"
       }
     },
   },

96afb1fb849e06a4f7429e414ab740d832735741
aureltbe <aureltrouble@gmail.com>
2022-01-20 17:32:08 +0100

    UI Design WIP 03

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -15,22 +15,22 @@ export interface IProject {
 const ProjectCard = ({project}: {project:IProject}) => {
     const {title, image, description, tags, source, visit, id} = project
     return (
-        <div className="">
+        <div className="claybg">
             <div className='relative mb-6 overflow-hidden shadow-md pb-80'>
                 <img
                     alt={title}
                     src={image}
-                    className='absolute object-cover object-top w-full rounded-t-lg shadow-lg h-80 lg:rounded-lg'
+                    className='absolute object-cover object-top w-full rounded-t-2xl shadow-lg h-80'
                 />
             </div>
-            <h3 className='mb-2 font-semibold text-xl'>
+            <h3 className='mb-2 mx-4 font-semibold text-xl'>
                 {title}
             </h3>
             <hr />
-            <div>
+            <div className='mx-4'>
                 {description}
             </div>
-            <div>
+            <div className='mx-4'>
                 <div className="flex justify-center mt-2">Techs :</div>
                 <div className='flex justify-between'>
                     {tags.map((tag, i) => (
@@ -40,14 +40,14 @@ const ProjectCard = ({project}: {project:IProject}) => {
                     ))}
                 </div>
             </div>
-            <div className='flex my-6 justify-around'>
+            <div className='flex my-6 justify-around mx-2'>
                 <Link href={visit}>
-                    <a target='_blank' className="bg-teal-500 hover:bg-teal-700 py-4 px-8 rounded-full shadow-xl font-semibold">
+                    <a target='_blank' className="bg-teal-500 hover:bg-teal-700 text-white py-4 px-8 rounded-full shadow-xl font-semibold">
                         Code
                     </a>
                 </Link>
                 <Link href={source}>
-                    <a target='_blank' className="bg-teal-500 hover:bg-teal-700 py-4 px-8 rounded-full shadow-xl font-semibold">
+                    <a target='_blank' className="bg-teal-500 hover:bg-teal-700 text-white py-4 px-8 rounded-full shadow-xl font-semibold">
                         Source
                     </a>
                 </Link>
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -5,7 +5,7 @@ import ProjectCard from './ProjectCard';
 const Projects = () => {
     return (
         <section id="projets" className="mx-10 lg:mx-20">
-            <div className="text-white grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 pb-10">
                 {projects.map(project => (
                     <ProjectCard project={project} key={project.title}/>
                 ))}
